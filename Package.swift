5fd2575e19d49d262752fd4d8e21a53833e0151a
cbepxbeo <cbepxbeo@mail.ru>
2024-11-03 18:00:33 +0300

    Update Package.swift
    added dependencies

--- Package.swift
+++ Package.swift
@@ -10,10 +10,17 @@ let package = Package(
             name: "ArithmeticExpression",
             targets: ["ArithmeticExpression"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/cbepxbeo/arithmetic-foundation.git", from: "1.0.0"),
+        .package(url: "https://github.com/cbepxbeo/arithmetic-difficulty.git", from: "0.0.1"),
+    ],
     targets: [
         .target(
             name: "ArithmeticExpression",
-            dependencies: [],
+            dependencies: [
+                .product(name: "ArithmeticFoundation", package: "arithmetic-foundation"),
+                .product(name: "ArithmeticDifficulty", package: "arithmetic-difficulty"),
+            ],
             path: "Sources"
         ),
         .testTarget(
