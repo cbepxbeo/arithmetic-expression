3eaec1839844377999ba950734e3d8cdf4ced7c2
cbepxbeo <cbepxbeo@mail.ru>
2024-11-10 09:43:20 +0300

    Decorate
    fix file comment
    makeRandom moved to file

--- Sources/Expression+Static+Methods+Make.swift
+++ Sources/Expression+Static+Methods+Make.swift
@@ -2,15 +2,6 @@
  
  |---------------------------------------------------------------------------------------
  |  Copyright The Arithmetic-expression Contributors.
- |  Licensed under the Apache License, Version 2.0 (the "License");
- |  you may not use this file except in compliance with the License.
- |  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- |
- |  Unless required by applicable law or agreed to in writing, software
- |  distributed under the License is distributed on an "AS IS" BASIS,
- |  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- |  See the License for the specific language governing permissions and
- |  limitations under the License.
  |---------------------------------------------------------------------------------------
  |  File: Expression+Static+Methods+Make.swift
  |  Created by: Egor Boyko
@@ -41,37 +32,4 @@ extension Expression {
                 []
             }
     }
-    
-    
-    static func makeRandom(
-        classifier: ArithmeticFoundation.Classifier,
-        operator: ArithmeticFoundation.Operator) -> Set<Expression> {
-            func assembly(difficulty: Difficulty, required: Int, input: inout Set<Expression>){
-                let startCount = input.count
-                while input.count < startCount + required {
-                    let random = Generator.random(
-                        difficulty: difficulty,
-                        classifier: classifier,
-                        operator: `operator`
-                    )
-                    input.insert(
-                        Expression(
-                            left: Element(random.0),
-                            right: Element(random.1),
-                            operator: `operator`
-                        )
-                    )
-                }
-            }
-            var output: Set<Expression> = []
-            
-            assembly(difficulty: .extraHard, required: 2, input: &output)
-            assembly(difficulty: .hard, required: 3, input: &output)
-            assembly(difficulty: .medium, required: 3, input: &output)
-            assembly(difficulty: .easy, required: 2, input: &output)
-            
-            return output
-    }
-    
-    
 }
